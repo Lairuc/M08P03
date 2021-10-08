72cc42b48963aecdf6fab1dbcfc452ed79e1d268
Lairuc <curial@elpuig.xeill.net>
2021-10-08 16:04:19 +0200

    3rd C&P added Options Menu

--- app/src/main/java/COR/M08/MainActivity.java
+++ app/src/main/java/COR/M08/MainActivity.java
@@ -5,9 +5,9 @@ import androidx.navigation.NavController;
 import androidx.navigation.fragment.NavHostFragment;
 import androidx.navigation.ui.AppBarConfiguration;
 import androidx.navigation.ui.NavigationUI;
-
 import android.os.Bundle;
-
+import android.view.Menu;
+import android.view.MenuItem;
 import java.util.Objects;
 
 import COR.M08.databinding.ActivityMainBinding;
@@ -15,6 +15,9 @@ import COR.M08.databinding.ActivityMainBinding;
 public class MainActivity extends AppCompatActivity {
 
     ActivityMainBinding binding;
+    //ActivityOptionsBinding binding2;
+    private NavController navController;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -31,5 +34,18 @@ public class MainActivity extends AppCompatActivity {
         NavController navController = ((NavHostFragment) Objects.requireNonNull(getSupportFragmentManager().findFragmentById(R.id.nav_host_fragment))).getNavController();
         NavigationUI.setupWithNavController(binding.navView, navController);
         NavigationUI.setupWithNavController(binding.toolbar, navController, appBarConfiguration);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.options_menu, menu);
+        return true;
+    }
+
+    //navController = ((NavHostFragment) Objects.requireNonNull(getSupportFragmentManager().findFragmentById(R.id.nav_host_fragment))).getNavController();
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        return NavigationUI.onNavDestinationSelected(item, navController)
+                || super.onOptionsItemSelected(item);
     }
 }
